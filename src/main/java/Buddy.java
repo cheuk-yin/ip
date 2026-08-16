4ab8413f3ed38254cc5f83f8e767d176eda907a2
Cheuk Yin <cheukyin04@gmail.com>
2026-08-16 22:20:52 +0800

    Changed chtabot name from Duke to Buddy

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,10 @@
+public class Buddy {
+    public static void main(String[] args) {
+        String banner = " ____   _   _  ____   ____  __   __\n"
+                + "| __ ) | | | ||  _ \\ |  _ \\ \\ \\ / /\n"
+                + "|  _ \\ | | | || | | || | | | \\ V / \n"
+                + "| |_) || |_| || |_| || |_| |  | |  \n"
+                + "|____/  \\___/ |____/ |____/   |_|  \n";
+        System.out.println(banner);
+    }
+}

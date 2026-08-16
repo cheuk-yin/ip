12ad113fc238c8686033490ac1d6b49aa10224ba
Cheuk Yin <cheukyin04@gmail.com>
2026-08-16 22:31:53 +0800

    Updated initial code to simply greet user and exit

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,10 +1,18 @@
 public class Buddy {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " ____   _   _  ____   ____  __   __\n"
                 + "| __ ) | | | ||  _ \\ |  _ \\ \\ \\ / /\n"
                 + "|  _ \\ | | | || | | || | | | \\ V / \n"
                 + "| |_) || |_| || |_| || |_| |  | |  \n"
                 + "|____/  \\___/ |____/ |____/   |_|  \n";
+        System.out.println(LINE);
         System.out.println(banner);
+        System.out.println("Hello! I'm Buddy.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }

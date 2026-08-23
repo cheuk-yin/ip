01b496564bca7ad6775eefadddba226e75740449
Cheuk Yin <cheukyin04@gmail.com>
2026-08-23 21:49:11 +0800

    Added echo function to Buddy

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Buddy {
     private static final String LINE = "____________________________________________________________";
 
+    public static void printMessage(String msg) {
+        System.out.println(LINE);
+        System.out.println(msg);
+        System.out.println(LINE);
+    }
     public static void main(String[] args) {
         String banner = " ____   _   _  ____   ____  __   __\n"
                 + "| __ ) | | | ||  _ \\ |  _ \\ \\ \\ / /\n"
@@ -12,7 +19,14 @@ public class Buddy {
         System.out.println("Hello! I'm Buddy.");
         System.out.println("What can I do for you?");
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.equals("bye")) {
+                printMessage("Bye. Hope to see you again soon!");
+                break;
+            }
+            printMessage(line);
+        }
     }
 }

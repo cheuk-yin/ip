04a5bd6fa90e20d6597622ce9e078bc9f0dd284b
Cheuk Yin <cheukyin04@gmail.com>
2026-09-19 18:05:58 +0800

    Add Javadoc to BuddyException constructor

--- src/main/java/buddy/exception/BuddyException.java
+++ src/main/java/buddy/exception/BuddyException.java
@@ -5,6 +5,12 @@ package buddy.exception;
  * e.g. because a required part of the input was missing or malformed.
  */
 public class BuddyException extends Exception {
+
+    /**
+     * Creates a new BuddyException with the given message.
+     *
+     * @param message the text to show the user explaining what went wrong
+     */
     public BuddyException(String message) {
         super(message);
     }
